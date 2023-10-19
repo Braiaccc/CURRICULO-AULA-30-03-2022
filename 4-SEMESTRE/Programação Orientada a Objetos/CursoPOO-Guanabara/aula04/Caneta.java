7428ce3b2a5a3579059d845a4ea5d4739da45773
Braiaccc <bryanarm985@gmail.com1git config --global user.name ''Braiaccc>
2023-10-19 14:43:37 -0300

    Add-ap2

--- 4-SEMESTRE/Programação Orientada a Objetos/CursoPOO-Guanabara/aula04/Caneta.java
+++ 4-SEMESTRE/Programação Orientada a Objetos/CursoPOO-Guanabara/aula04/Caneta.java
@@ -1,8 +1,18 @@
 public class Caneta {
     public String modelo;
-    private int ponta;
+    private float ponta;
+    private String cor;
+    private boolean tampada;
+
+    public Caneta(String m, String c, float p){
+        this.setModelo(m);
+        this.setCor(c);
+        this.setPonta(p);
+        this.tampar();
+    }
 
     public String getModelo(){
+
         return this.modelo;
     }
     public void setModelo(String m){
@@ -15,4 +25,28 @@ public class Caneta {
     public void setPonta(float p){
         this.ponta = p;
     }
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public void tampar(){
+        this.tampada = true;
+    }
+
+    public void destampar(){
+        this.tampada = false;
+    }
+
+    public void status(){
+        System.out.println("Sobre a caneta");
+        System.out.println("MODELO " + this.getModelo());
+        System.out.println("Ponta " + this.getPonta());
+        System.out.println("Cor " + this.cor);
+        System.out.println("Tampada " + this.tampada);
+
+    }
 }
+
---
+++ 4-SEMESTRE/Programação Orientada a Objetos/CursoPOO-Guanabara/aula04/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("bic", "Amarelo", 2.2f);
+        c1.status();
+        Caneta c2 = new Caneta("kkk", "verde", 2.0f);
+        c2.status();
+    }
+}
